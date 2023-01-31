0edbe5c496d628f6c690dd8e071a2c9b19e330fc
Dennis Nguyen <dnsnguy08@gmail.com>
2023-01-30 22:43:39 -0800

    enable google and github oauths & login modal logic

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -2,6 +2,9 @@ import { authModalState } from "@/src/atoms/authModalAtom";
 import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
+import { FIREBASE_ERRORS } from "@/src/firebase/errors";
 
 type LoginProps = {};
 
@@ -12,8 +15,15 @@ const Login: React.FC<LoginProps> = () => {
     password: "",
   });
 
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
   // Firebase logic
-  const onSubmit = () => {};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(loginForm.email, loginForm.password);
+  };
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     // update form state
@@ -69,7 +79,17 @@ const Login: React.FC<LoginProps> = () => {
         }}
         bg="gray.50"
       />
-      <Button width="100%" height="36px" mt={2} mb={2} type="submit">
+      <Text textAlign="center" fontSize="10pt" color="red">
+        {FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
+      <Button
+        width="100%"
+        height="36px"
+        mt={2}
+        mb={2}
+        type="submit"
+        isLoading={loading}
+      >
         Log In
       </Button>
       <Flex fontSize="9pt" justifyContent="center">
--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,18 +1,41 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import {
+  useSignInWithGoogle,
+  useSignInWithGithub,
+} from "react-firebase-hooks/auth";
+
+import { auth } from "@/src/firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+
+  const [signInWithGithub, githubUser, githubLoading, githubError] =
+    useSignInWithGithub(auth);
+
   // variant theme from chakra button.ts
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={googleLoading}
+        onClick={() => signInWithGoogle()}
+      >
         <Image src="/images/googlelogo.png" height="20px" mr={4} />
         Continue With Google
       </Button>
-      <Button variant="oauth">
+      <Button
+        variant="oauth"
+        isLoading={githubLoading}
+        onClick={() => signInWithGithub()}
+      >
         <Image src="/images/github-mark.png" height="20px" mr={4} />
         Continue With GitHub
       </Button>
+      {googleError && <Text>{googleError.message}</Text>}
+      {githubError && <Text>{githubError.message}</Text>}
     </Flex>
   );
 };
